cc6b42fa27e2ef57c4113019fb470233acf860a9
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-12-22 08:17:23 +0200

    simplify http server

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import (
 )
 
 func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = io.WriteString(w, "Hello, Skaffold!")
-	}
-	http.HandleFunc("/hello", helloHandler)
+	})
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
