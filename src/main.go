9c7d21d477c63c869d3ec41926af06efe8d5e0ba
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-01-14 21:35:31 +0200

    add additional test endpoint

--- src/main.go
+++ src/main.go
@@ -9,7 +9,10 @@ import (
 func main() {
 	log.Println("starting server...")
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		_, _ = io.WriteString(w, "Hello, Skaffold!\n")
+		_, _ = io.WriteString(w, "It's Skaffold!\n")
+	})
+	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+		_, _ = io.WriteString(w, "Hello Skaffold!\n")
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
