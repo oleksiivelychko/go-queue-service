e06f597895f13f46ce1dcd6147335f7fbd14ab53
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-01-15 22:22:11 +0200

    add trailing slash

--- src/main.go
+++ src/main.go
@@ -11,7 +11,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = io.WriteString(w, "It's Skaffold!\n")
 	})
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/hello/", func(w http.ResponseWriter, req *http.Request) {
 		_, _ = io.WriteString(w, "Hello Skaffold!\n")
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
