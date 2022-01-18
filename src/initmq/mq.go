9a2f4b38e8385dea17d37c10b6a006683bdcedbc
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-01-18 08:23:26 +0200

    test rabbitmq channel

--- src/initmq/mq.go
+++ src/initmq/mq.go
@@ -3,16 +3,21 @@ package initmq
 import (
 	"fmt"
 	"github.com/streadway/amqp"
-	"log"
 )
 
 func Connection(host, user, pass, port string) (conn *amqp.Connection, err error) {
 	url := fmt.Sprintf("amqp://%s:%s@%s:%s", user, pass, host, port)
-
 	conn, err = amqp.Dial(url)
-	if err != nil {
-		log.Panicf("%s: %s", "Failed to connect to RabbitMQ", err)
-	}
-
 	return
+}
+
+func MakeQueue(ch *amqp.Channel, name string) (amqp.Queue, error) {
+	return ch.QueueDeclare(
+		name,  // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
 }
--- src/initmq/mq_test.go
+++ src/initmq/mq_test.go
@@ -12,3 +12,18 @@ func TestMQConnection(t *testing.T) {
 		t.Errorf("unable to init RabbitMQ connection: %s", err)
 	}
 }
+
+func TestMakeQueue(t *testing.T) {
+	LoadEnv()
+
+	conn, _ := MQ()
+	ch, err := conn.Channel()
+	if err != nil {
+		t.Errorf("unable to create RabbitMQ channel: %s", err)
+	}
+
+	_, err = MakeQueue(ch, "test")
+	if err != nil {
+		t.Errorf("unable to make RabbitMQ queue: %s", err)
+	}
+}
